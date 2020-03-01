ca217c8864ce06e9b28cb97616fab597ea8e9db4
a2696870402 <2696870402@qq.com>
2020-03-01 23:05:24 +0800

    学习。2020.03.01

--- src/cn/com/hanshu/test01.java
+++ src/cn/com/hanshu/test01.java
@@ -1,5 +1,7 @@
 package cn.com.hanshu;
 
+import org.junit.Test;
+
 import java.util.stream.Stream;
 
 public class test01 {
@@ -24,6 +26,57 @@ public class test01 {
         //dropWhile 如果Stream是有序的，那么返回除了最长命中序列的其他序列，
         //          如果是无序的，那么返回所有未命中的序列
 
+    }
+    @Test
+    public void Test1(){
+        int jj=1;
+        char j,q='A';
+        j=(char)(q+jj-1);  // A+1=B  A+2=C 依次
+        System.out.println(j);
+        char e=65;
+        System.out.println(e);
+    }
+    @Test
+    public void Test2(){
+        char a='A';
+        int i=0;
+        while(true){
+            char ee=(char)(a+i);
+            System.out.print(ee);
+            i++;
+            if(ee=='Z'){
+                a='a';
+                i=0;
+            }
+            if(ee=='z'){
+                a='0';
+                i=0;
+            }
+            if(ee=='9'){
+                break;
+            }
+
+        }
+    }
+    @Test
+    //最长上升序列
+    public void Test3(){
+        int n;
+        //输出n
+        int[] day=new int[10000];
+        //输入操作
+        int count=1;
+        int max_num=1;
+        for(int i=1;i<n;i++){
+            if(day[i]>day[i-1]){
+                count++;
+            }
+            else {
+                max_num=max_num<count?count:max_num;
+                count=1;
+            }
+        }
+        System.out.println(max_num);
 
     }
 }
